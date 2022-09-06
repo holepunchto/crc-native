b680f1b1170600d2a7d06d8a56aa2d7533a6d694
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-06 14:51:09 +0200

    Remove segfault handler

--- test.mjs
+++ test.mjs
@@ -2,10 +2,6 @@ import test from 'brittle'
 
 import { crc32 } from './index.js'
 
-import SegfaultHandler from 'segfault-handler'
-
-SegfaultHandler.registerHandler()
-
 test('crc32', (t) => {
   t.is(crc32(Buffer.from('abcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefgh')), 0x6b64f5d)
 })
