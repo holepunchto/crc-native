3a0f0fa3083b72a8dbd98d1eb54f4ebe99da618c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-06 13:45:03 +0200

    Revert "Try with segfault handler"
    This reverts commit 19c5b2b967fe83d28d573b528bbc586d3dade664.

--- test.mjs
+++ test.mjs
@@ -2,10 +2,6 @@ import test from 'brittle'
 
 import { crc32 } from './index.js'
 
-import segfaultHandler from 'node-segfault-handler'
-
-segfaultHandler.registerHandler()
-
 test('crc32', (t) => {
   t.is(crc32(Buffer.from('abcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefgh')), 0x6b64f5d)
 })
