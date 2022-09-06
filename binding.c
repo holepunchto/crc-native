3d9ef31d9c28e9ba700f6e2f71ee29724fee071b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-06 13:14:01 +0200

    Update `libcrc`

--- binding.c
+++ binding.c
@@ -1,13 +1,10 @@
 #include <crc.h>
 #include <napi-macros.h>
 #include <node_api.h>
-#include <stdio.h>
 
 NAPI_METHOD(crc32_napi) {
   NAPI_ARGV(1);
   NAPI_ARGV_BUFFER_CAST(uint8_t *, buf, 0);
-
-  printf("%.*s %lu\n", (int) buf_len, buf, buf_len);
 
   NAPI_RETURN_UINT32(crc32(buf, buf_len));
 }
