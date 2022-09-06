a857cebe3ee9f75d3cdcad964b27284e8c1fa353
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-06 13:04:37 +0200

    Print buffer

--- binding.c
+++ binding.c
@@ -1,12 +1,15 @@
 #include <crc.h>
 #include <napi-macros.h>
 #include <node_api.h>
+#include <stdio.h>
 
 NAPI_METHOD(crc32_napi) {
   NAPI_ARGV(1);
-  NAPI_ARGV_BUFFER(buf, 0);
+  NAPI_ARGV_BUFFER_CAST(uint8_t *, buf, 0);
 
-  NAPI_RETURN_UINT32(crc32((uint8_t *) buf, buf_len));
+  printf("%.*s %lu\n", (int) buf_len, buf, buf_len);
+
+  NAPI_RETURN_UINT32(crc32(buf, buf_len));
 }
 
 NAPI_INIT() {
