ab642b58d1bbec9f1b3124eb8e912eac5d25a929
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-06 13:54:58 +0200

    Try empty string

--- test.mjs
+++ test.mjs
@@ -3,5 +3,5 @@ import test from 'brittle'
 import { crc32 } from './index.js'
 
 test('crc32', (t) => {
-  t.is(crc32(Buffer.from('abcdefghabcdefghabcdefghabcdefgh')), 0x3e354ff8)
+  t.is(crc32(Buffer.from('')), 0)
 })
