156fa184441545f05259864d56031adf56ff6e49
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-06 14:20:47 +0200

    Try different segfault handler

--- test.mjs
+++ test.mjs
@@ -2,6 +2,10 @@ import test from 'brittle'
 
 import { crc32 } from './index.js'
 
+import SegfaultHandler from 'segfault-handler'
+
+SegfaultHandler.registerHandler()
+
 test('crc32', (t) => {
   t.is(crc32(Buffer.from('abcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefgh')), 0x6b64f5d)
 })
